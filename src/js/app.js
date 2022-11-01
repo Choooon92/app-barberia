5f53de5c304e778293b16117524a7875cbb7e7c9
Choooon92 <javi_arc@hotmail.com>
2022-11-01 14:54:11 -0300

    Comit Barberia

---
+++ src/js/app.js
@@ -0,0 +1,213 @@
+let pagina = 1;
+
+const cita = {
+    nombre: '',
+    fecha: '',
+    hora: '',
+    servicios: []
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    iniciarApp();
+});
+
+function iniciarApp() {
+    mostrarServicios();
+
+    // Resalta el DIV actual segun el tab que se presione
+    mostrarSeccion();
+
+    // Oculta o mestra una seccion segun "" ""  ""
+    cambiarSeccion();
+
+    // Paginacion siguiente y anterior
+    paginaSiguiente();
+
+    paginaAnterior();
+
+    // Comprueba la pagina actual para ocultar o mostrar la paginacion
+    botonesPaginador();
+
+    // Muestra el resumen de la cita (o mensaje de error al no pasar validacion)
+    mostrarResumen();
+}
+
+function mostrarSeccion() {
+
+    // eliminar mostras-seccion de la seleccion anterior
+    const seccionAnterior = document.querySelector('.mostrar-seccion');
+    if ( seccionAnterior ) {
+        seccionAnterior.classList.remove('mostrar-seccion');
+    }
+
+    const seccionActual = document.querySelector(`#paso-${pagina}`);
+    seccionActual.classList.add('mostrar-seccion');
+
+    // Eliminar la clase de actual en el tab anterior
+    const tabAnterior = document.querySelector('.tabs .actual');
+    if (tabAnterior) {
+        tabAnterior.classList.remove('actual');
+    }
+     
+    // Resalta el Tab Actual
+    const tab = document.querySelector(`[data-paso="${pagina}"]`);
+    tab.classList.add('actual');
+}
+
+function cambiarSeccion() {
+    const enlaces = document.querySelectorAll('.tabs button');
+
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', e => {
+            e.preventDefault();
+            pagina = parseInt(e.target.dataset.paso);
+
+            // Lamar la funcion de mostrar seccion
+            mostrarSeccion();
+
+            botonesPaginador();
+        })
+    })
+}
+
+async function mostrarServicios() {
+    try {
+        const resultado = await fetch('./servicios.json');
+        const db = await resultado.json();
+        
+        const { servicios } = db;
+
+        // Generar el HTML
+        servicios.forEach(servicio => {
+            const { id, nombre, precio } = servicio;
+            
+            // DOM Scripting
+            // Generar nombre de servicio
+            const nombreServicio = document.createElement('P');
+            nombreServicio.textContent = nombre;
+            nombreServicio.classList.add('nombre-servicio');
+
+            //Generar precio de servicio
+            const precioServicio = document.createElement('P');
+            precioServicio.textContent = `$ ${precio}`;
+            precioServicio.classList.add('precio-servicio');
+
+            // Generar div contenedor de servicio
+            const servicioDiv = document.createElement('DIV');
+            servicioDiv.classList.add('servicio');
+            servicioDiv.dataset.idServicio = id;
+
+            // Selecciona un servicio para la cita
+            servicioDiv.onclick = seleccionarServicio;
+
+            // Inyectar precio y nombre al div
+            servicioDiv.appendChild(nombreServicio);
+            servicioDiv.appendChild(precioServicio);
+
+            // Inyectarlo en el HTML
+            document.querySelector('#servicios').appendChild(servicioDiv);
+        });
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+function seleccionarServicio(e) {
+
+    let elemento;
+    // Forzar que el elemento al cual le damos click sea el DIV
+    if (e.target.tagName === 'P') {
+        elemento = e.target.parentElement;
+    } else {
+        elemento = e.target;
+    }
+
+    if (elemento.classList.contains('seleccionado')) {
+        elemento.classList.remove('seleccionado');
+
+        const id = parseInt( elemento.dataset.idServicio );
+
+        eliminarServicio(id);
+    } else {
+        elemento.classList.add('seleccionado');
+
+        const servicioObj = {
+            id: parseInt( elemento.dataset.idServicio ),
+            nombre: elemento.fisrtElementChild.textContent,
+            precio: elemento.fisrtElementChild.nextElementSibling.textContent
+        }
+
+        // console.log(servicioObj);
+        agregarServicio(servicioObj);
+    }
+}
+
+function eliminarServicio(id) {
+    const { servicios } = cita;
+    cita.servicios = servicios.filter( servicio => servicio.id !== id);
+}
+
+function agregarServicio(servicioObj) {
+    const { servicios } = cita;
+
+    cita.servicios = [...servicios, servicioObj];
+}
+
+function paginaSiguiente() {
+    const paginaSiguiente = document.querySelector('#siguiente');
+    paginaSiguiente.addEventListener('click', () => {
+        pagina++;
+
+        console.log(pagina);
+
+        botonesPaginador();
+    });
+}
+
+function paginaAnterior() {
+    const paginaAnterior = document.querySelector('#anterior');
+    paginaAnterior.addEventListener('click', () => {
+        pagina--;
+
+        console.log(pagina);
+
+        botonesPaginador();
+    });
+}
+
+function botonesPaginador() {
+    const paginaSiguiente = document.querySelector('#siguiente');
+    const paginaAnterior = document.querySelector('#anterior');
+
+    if (pagina === 1) {
+        paginaAnterior.classList.add('ocultar');
+    } else if (pagina === 3) {
+        paginaSiguiente.classList.add('ocultar');
+        paginaAnterior.classList.remove('ocultar');
+    } else {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    }
+
+    mostrarSeccion();
+}
+
+function mostrarResumen() {
+    //Destructuring
+    const { nombre, fecha, hora, servicios } = cita;
+
+    // Seleccionar el resumen
+    const resumenDiv = document.querySelector('.contenido-resumen')
+
+    //Validacion de objeto
+
+    if (Object.values(cita).includes('')) {
+        const noServicios = document.createElement('P');
+        noServicios.textContent = 'Faltan datos de servicios, nombre, fecha u hora';
+
+        noServicios.classList.add('invalidar-cita');
+
+        // agregando a resumen Div
+        resumenDiv.appendChild(noServicios);
+    }
+}
